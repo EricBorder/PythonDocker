7a7d049420021e6bf77937d5cceaae694c86130a
EricBorder <magicsergy@gmail.com>
2023-03-03 12:26:49 +0100

    Initial commit

---
+++ app/miscript.py
@@ -0,0 +1,11 @@
+from pytube import YouTube
+
+#link = input("Enter the link: ")
+yt = YouTube("https://www.youtube.com/watch?v=TPbW8qCvuLA")
+
+print("Titulo: ",yt.title)
+print("Numero de visitas: ",yt.views)
+print("Longitud del video: ",yt.length,"segundos")
+print("Descripción: ",yt.description)
+print("Valoraciones: ",yt.rating)
+yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
